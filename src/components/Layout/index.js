729542a1be7b0ecce741bb3f8ad980a841f3b161
PavanUmmagani07 <pavankalyanU@outlook.com>
2023-05-03 04:14:16 +0000

    Layout Generator

---
+++ src/components/Layout/index.js
@@ -0,0 +1,16 @@
+import Header from '../Header'
+import Footer from '../Footer'
+import Body from '../Body'
+import './index.css'
+
+const Layout = () => (
+  <div className="layout-container">
+    <div className="responsive-container">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  </div>
+)
+
+export default Layout
